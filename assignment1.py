9d7dd63fd894dfeca6286b5dbdf2342ccde65105
SAKE CHAITANYA LAKSHMI <61873017+sakechaithanya@users.noreply.github.com>
2023-02-08 21:15:25 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,14 @@
-#mirror dimension
+2#mirror dimension
 sake= input("enter the input ")
 print(sake[::-1])
+3#odd n even numbers seies
+sake =input("enter the series").split(',')
+count=0
+count1=0
+for i in sake:
+   if int(i)%2==0:
+      count+=1
+   else :
+      count1+=1
+print(count," count of even number")
+print(count1,"count of odd number")
